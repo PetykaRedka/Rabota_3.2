a1af369e576e46df4b19b0f0d7a0ad22f07152c4
PetykaRedka <Rozenbaum90@yandex.ru>
2024-09-29 17:11:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+        Square square = new Square();
+        System.out.println(square);
+        Circle circle = new Circle();
+        System.out.println(circle);
+    }
+}
